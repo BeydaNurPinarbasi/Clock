1482bf265ce53de4770a5e8637ed1c68dfd24e69
Beyda Nur PINARBAŞI <140889636+BeydaNurPinarbasi@users.noreply.github.com>
2024-05-10 01:27:45 +0300

    Add files via upload

---
+++ Clock/Clock/Program.cs
@@ -0,0 +1,17 @@
+﻿
+class Program
+{
+  static void Main()
+  {
+    // Get the current time
+    DateTime now = DateTime.Now;
+
+    // Get the hour and minute
+    int hour = now.Hour;
+    int minute = now.Minute;
+
+    // Display the hour and minute
+    Console.WriteLine($"what time is it?");
+    Console.WriteLine($"{hour}:{minute}");
+  }
+}
