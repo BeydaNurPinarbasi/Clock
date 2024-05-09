55f0f43bb34929e9256af5dc2270ac0758f52d82
Beyda Nur PINARBAŞI <140889636+BeydaNurPinarbasi@users.noreply.github.com>
2024-05-10 01:29:05 +0300

    clock
    finish

--- Clock/Clock/Program.cs
+++ Clock/Clock/Program.cs
@@ -3,14 +3,10 @@ class Program
 {
   static void Main()
   {
-    // Get the current time
+   
     DateTime now = DateTime.Now;
-
-    // Get the hour and minute
     int hour = now.Hour;
     int minute = now.Minute;
-
-    // Display the hour and minute
     Console.WriteLine($"what time is it?");
     Console.WriteLine($"{hour}:{minute}");
   }
